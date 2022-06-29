f49876673ab2bcc09ddfddffb85066f1cf03af41
svsarthakjain <vishu2952001@gmail.com>
2022-06-29 10:16:55 +0530

    hii

---
+++ src/aditya1/PrimeFact.java
@@ -0,0 +1,12 @@
+package aditya1;
+
+public class PrimeFact {
+	 public static void main(String args[]){  
+		  int i,fact=1;  
+		  int number=10;
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }  
+}
